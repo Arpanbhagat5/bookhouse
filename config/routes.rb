d2749c98e44c6c0113ad1c5e2d9c8566c971914e
Bhagat <arpanbhagat5@gmail.com>
2022-12-16 13:54:46 +0900

    Add review routes under books route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories, only: %i[index create destroy]
-      resources :books, only: %i[index create show update destroy]
+
+      resources :books, only: %i[index create show update destroy] do
+        resources :reviews, only: %i[index create show update destroy]
+      end
+
     end
   end
 end
